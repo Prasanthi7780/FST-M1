730d99bf353a2c707733059d2b6a2c2785c9b2e8
Prasanthi7780 <shanthip7780@gmail.com>
2025-12-19 16:07:36 +0530

    Refactor Addable interface and update main method

--- activity12.java
+++ activity12.java
@@ -1,18 +1,14 @@
-interface Addable {
-    int add(int a, int b);
+public class Activity12 {
+
+	public static void main(String[] args) {
+		Addable ad1 = (a,b) -> (a+b);
+		System.out.println(ad1.add(15, 10));
+		
+		Addable ad2 = (int a, int b) -> {
+			return (a+b);
+		};
+		System.out.println(ad2.add(50, 70));
+
+	}
 }
-
-public class activity12 {
-    public static void main(String[] args) {
-
-        // Lambda expression without return keyword.
-        Addable ad1 = (a, b) -> (a + b);
-        System.out.println(ad1.add(10, 20));
-
-        // Lambda expression with return keyword.
-        Addable ad2 = (int a, int b) -> {
-            return (a + b);
-        };
-        System.out.println(ad2.add(100, 200));
-    }
 }
