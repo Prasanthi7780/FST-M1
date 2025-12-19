65d07463ce56b06d265a7767589220e8ecaf18a4
Prasanthi7780 <shanthip7780@gmail.com>
2025-12-19 16:34:48 +0530

    Add files via upload

---
+++ FST-Cucumber/src/test/java/activities/Activityfeature.java
@@ -0,0 +1,14 @@
+package activities;
+
+public class Activityfeature {
+
+	public Activityfeature() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
